46854dc015da221184e25dba8beb6f7b352f7b84
sajitha11 <sajithasaji1109@gmail.com>
2024-12-12 12:24:53 +0530

    changes in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/React-Todo/', // Replace 'repo-name' with your GitHub repository name
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
